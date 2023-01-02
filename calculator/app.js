104118a6f8713a9f740eeb8a2dbe072de524c711
jomobrain1 <jomobrain1@gmail.com>
2023-01-02 01:39:11 -0800

    calculator

---
+++ calculator/app.js
@@ -0,0 +1,29 @@
+console.log('jomobrain1');
+let btns=document.querySelectorAll('p')
+let output=document.querySelector('.output')
+let equal=document.querySelector('.equal')
+let del=document.querySelector('.delete')
+
+btns.forEach(btn=>{
+    btn.onclick=(e)=>{
+       output.innerText+=e.target.innerText
+    //    console.log(e.target.innerText);
+    }
+})
+
+equal.onclick=()=>{
+    try {
+        console.log( eval(output.innerText));  
+    } catch (error) {
+        if(error){
+            output.innerText="! Error !"  
+            output.classList.add('error')      
+        }
+        console.log(error);
+    }
+ 
+}
+del.onclick=()=>{
+    
+    output.innerText= output.innerText.slice(0,-1)
+}

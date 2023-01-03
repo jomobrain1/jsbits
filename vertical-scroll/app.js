4ae49f59509bd3905ad5485009e6863e290bd15e
jomobrain1 <jomobrain1@gmail.com>
2023-01-02 22:58:27 -0800

    changes on vertical scroll

--- vertical-scroll/app.js
+++ vertical-scroll/app.js
@@ -1,5 +1,18 @@
 console.log("jomobrain1");
-let btn=document.querySelectorAll('a')
-btn[1].onclick=()=>{
-    console.log('you');
+let btn=document.querySelector('button')
+let container=document.querySelector('.container')
+let links=document.querySelectorAll('a')
+let boxes=document.querySelectorAll('.box')
+let i=0
+btn.onclick=()=>{
+    i++
+    if(i > boxes.length){
+        i=0
+    }
+    console.log(i*100);
+    console.log(boxes.length);
+    
+    // box.style.transform = "translateY(-200vh)";
+    container.style.transform = `translateY(-${i*100}vh)`;
+    console.log(container);
 }

5d6a29d1c54c7b1650516bd7b630ebf23ae0e6ec
jomobrain1 <jomobrain1@gmail.com>
2023-01-05 04:38:17 -0800

    mega menu

---
+++ mega/app.js
@@ -0,0 +1,9 @@
+console.log('jomobrain1');
+
+let slide=document.querySelector('#slide')
+let links=document.querySelector('.links')
+slide.onclick=()=>{
+console.log(links);
+links.classList.toggle('left')
+}
+

802026904446e38522a122690d1c735497177658
jomobrain1 <jomobrain1@gmail.com>
2023-01-07 10:45:37 -0800

    accordion

--- accordion/app.js
+++ accordion/app.js
@@ -1,9 +1,1 @@
 console.log('jomobrain1');
-let accordLink=document.querySelectorAll('.accordion-link')
-
-accordLink.forEach(ac=>{
-    ac.onclick=(e)=>{
-        let btn=e.path[2].children[1]
-        btn.classList.toggle('none') 
-    }
-})
